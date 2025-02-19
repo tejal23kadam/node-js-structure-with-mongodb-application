c77ec96788905cc5fc447b52eb64100c2cf37bdb
tejal23kadam <tejalmchandlekar@gmail.com>
2025-02-19 12:47:40 +0530

    commit add

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -11,20 +11,13 @@ import CloseButton from 'react-bootstrap/CloseButton';
 function Header(props) {
 
     const [show, setShow] = useState(false);
-    const [color, setcolor1] = useState(false);
-    const handleClose = () => setShow(false);
+
+    const handleClose = () => setShow(!show);
     const handleShow = () => setShow(true);
 
     const [visibleSearchBar, setVisibleSearchBar] = useState(false);
-    const [navBarcolor, setcolor] = useState("redColor");
     const [activeLink, setActiveLink] = useState("Dashboard");
 
-    const click = () => {
-
-        setcolor1(!color);
-        console.log("color is " + color);
-        (color) ? setcolor("redColor") : setcolor("greenColor")
-    }
     const dispatch = useDispatch();
     const naviget = useNavigate();
     const user = useSelector((state) => state.auth.user);
@@ -39,92 +32,16 @@ function Header(props) {
     return (
         <div class="container-fluid">
             <div class="row ">
-
-                {/* main nav link section start */}
-                {/* <div class="d-flex border-bottom border-3">
-                    <div class="px-5 py-3 bd-highlight d-none d-lg-block col-xl-2 col-lg-3">
-                        <h1>LOGO</h1>
-                    </div>
-                    <button type="button" class="navbar-mobile-toggler border-0 d-block d-lg-none">
-                        <svg xmlns="http://www.w3.org/2000/svg" style={{ backgroundColor: "white" }} width="30" height="30" onClick={handleShow} class="bi bi-text-indent-right" viewBox="0 0 16 16">
-                            <path d="M2 3.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m10.646 2.146a.5.5 0 0 1 .708.708L11.707 8l1.647 
-                                                    1.646a.5.5 0 0 1-.708.708l-2-2a.5.5 0 0 1 0-.708zM2 6.5a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5m0 3a.5.5 
-                                                    0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5m0 3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5" />
-                        </svg>
-                    </button>
-                    <button className='d-none d-lg-block border-0' >
-                        <svg xmlns="http://www.w3.org/2000/svg" style={{ backgroundColor: "white" }} width="30" height="30" onClick={click} class="bi bi-text-indent-right" viewBox="0 0 16 16">
-                            <path d="M2 3.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m10.646 2.146a.5.5 0 0 1 .708.708L11.707 8l1.647 
-                                                    1.646a.5.5 0 0 1-.708.708l-2-2a.5.5 0 0 1 0-.708zM2 6.5a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5m0 3a.5.5 
-                                                    0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5m0 3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5" />
-                        </svg>
-                    </button>
-                    <div class="p-2 flex-grow-1 d-none d-lg-block">
-                        <div class="form-group has-search">
-                            <span class="bi bi-search form-control-feedback"></span>
-                            <input type="text" class="form-control" placeholder="Search" />
-                        </div>
-                    </div>
-                    <div class="p-lg-4 px-2 py-4 flex-grow-1 d-block d-lg-none">
-                        <svg xmlns="http://www.w3.org/2000/svg" onClick={() => setVisibleSearchBar(true)} width="20" height="20" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-                            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 
-                                        1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5
-                                         0 0 1 11 0" />
-                        </svg>
-                    </div>
-                    <div class=" py-4 px-2 ">
-                        <Link to="/"><i className="bi bi-bell-fill" onClick={handleShow}></i>                                         </Link>
-                    </div>
-                    <div class="py-4 px-2">
-                        <Link to="/"><i className='bi bi-gear'></i></Link>
-                    </div>
-                    <div class="py-1 px-2 ">
-                        <Dropdown>
-                            <Dropdown.Toggle variant="transparent" className='btn-outline-light text-dark '>
-                                <div class="profile">
-                                    <div class="img-box">
-                                        <img src="https://i.postimg.cc/BvNYhMHS/user-img.jpg" alt="some user image" />
-                                    </div>
-                                    <div class="user text-capitalize text-start d-none d-lg-block">
-                                        <h4>{(user !== null) ? (user.name) : ("admin")}</h4>
-                                        <p>FrontEnd Developer </p>
-                                    </div>
-                                </div>
-                            </Dropdown.Toggle>
-                            <Dropdown.Menu>
-                                <Dropdown.Item ><i class="bi bi-gear px-2"></i>Sign In</Dropdown.Item>
-                                <Dropdown.Item><i class="bi bi-gear px-2"></i>Sign Out</Dropdown.Item>
-                            </Dropdown.Menu>
-                        </Dropdown>
-                    </div>
-                </div>
-
-                {(visibleSearchBar) ? (
-                    <div className='search-section d-lg-none d-flex w-100 p-4' onClick={() => setVisibleSearchBar(false)}>
-                        <div class="has-search flex-grow-1 ">
-                            <span class="bi bi-search form-control-feedback p-2"></span>
-                            <input type="text" class="form-control" placeholder="Search" />
-                        </div>
-                        <CloseButton className="p-2" onClick={() => setVisibleSearchBar(false)} />
-                    </div>) : (<></>)}*/}
-
-
-                {/* main nav link section end  */}
-
-
-
-                {/* tejal */}
-
                 {/* offcanvas setion start */}
-                <div className='d-flex'>
-                    {(!show && !color) ? (
-                        <div className='px-4 py-2'>
+                <div className='d-flex '>
+                    {(!show) ? (
+                        <div className='px-2 mx-2 mx-md-0 px-md-3 py-3 d-none d-lg-block'>
                             <h1>L</h1>
                         </div>
                     ) :
                         (
                             <div>
-                                <div class="px-5 py-3 bd-highlight col-xl-2 ">
+                                <div class="px-5 py-3 bd-highlight col-xl-2 d-none d-lg-block ">
                                     <h1>LOGO</h1>
                                 </div>
 
@@ -136,15 +53,9 @@ function Header(props) {
 
                     {/* pages section start */}
 
-                    <button type="button" class="navbar-mobile-toggler border-0 d-block d-lg-none">
-                        <svg xmlns="http://www.w3.org/2000/svg" style={{ backgroundColor: "white" }} width="30" height="30" onClick={handleShow} class="bi bi-text-indent-right" viewBox="0 0 16 16">
-                            <path d="M2 3.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m10.646 2.146a.5.5 0 0 1 .708.708L11.707 8l1.647 
-                                                    1.646a.5.5 0 0 1-.708.708l-2-2a.5.5 0 0 1 0-.708zM2 6.5a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5m0 3a.5.5 
-                                                    0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5m0 3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5" />
-                        </svg>
-                    </button>
-                    <button className='d-none d-lg-block border-0' >
-                        <svg xmlns="http://www.w3.org/2000/svg" style={{ backgroundColor: "white" }} width="30" height="30" onClick={click} class="bi bi-text-indent-right" viewBox="0 0 16 16">
+
+                    <button className='border-0 bg-light' >
+                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" onClick={handleClose} class="bi bi-text-indent-right" viewBox="0 0 16 16">
                             <path d="M2 3.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m10.646 2.146a.5.5 0 0 1 .708.708L11.707 8l1.647 
                                                     1.646a.5.5 0 0 1-.708.708l-2-2a.5.5 0 0 1 0-.708zM2 6.5a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5m0 3a.5.5 
                                                     0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5m0 3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5" />
@@ -189,13 +100,13 @@ function Header(props) {
                         </Dropdown>
                     </div>
                     {(visibleSearchBar) ? (
-                    <div className='search-section d-lg-none d-flex w-100 p-4' onClick={() => setVisibleSearchBar(false)}>
-                        <div class="has-search flex-grow-1 ">
-                            <span class="bi bi-search form-control-feedback p-2"></span>
-                            <input type="text" class="form-control" placeholder="Search" />
-                        </div>
-                        <CloseButton className="p-2" onClick={() => setVisibleSearchBar(false)} />
-                    </div>) : (<></>)}
+                        <div className='search-section d-lg-none d-flex w-100 p-4'>
+                            <div class="has-search flex-grow-1 ">
+                                <span class="bi bi-search form-control-feedback p-2"></span>
+                                <input type="text" class="form-control" placeholder="Search" />
+                            </div>
+                            <CloseButton className="p-2" onClick={() => setVisibleSearchBar(false)} />
+                        </div>) : (<></>)}
 
 
                     {/* pages section end  */}
@@ -205,8 +116,9 @@ function Header(props) {
 
                 {/* offcanvas setion start */}
                 <div className='d-flex'>
-                    {(!show && !color) ? (
-                        <div className='px-3 py-2'>
+                    {(!show) ? (
+                        <div className='px-1 px-md-3 py-2 d-none d-lg-block'>
+
                             <nav id="navmenu" className="navmenu">
                                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                                     {props.navSections.map((section, i) => (
@@ -223,27 +135,38 @@ function Header(props) {
                         </div>
                     ) :
                         (
-                            <div style={{maxWidth:'200px'}}>
-                                <div className=' p-4' >
-                                    <div className="profile-img">
-                                        <img src={require('../images/User-Profile-PNG-Image.png')} alt="" className="img-fluid rounded-circle" />
-                                    </div>
-                                    <h1 className="sitename text-capitalize">{(user !== null) ? (user.name) : ("admin")}</h1>
-                                    <nav id="navmenu" className="navmenu">
-                                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                                            {props.navSections.map((section, i) => (
-                                                <li key={i} className="nav-item">
-                                                    <Link className={activeLink === section.secName ? "active" : ""}
-                                                        onClick={() => setActiveLink(section.secName)}
-                                                        to={section.linkTo} >
-                                                        <i className={section.icon}></i>
-                                                        {section.secName}
-                                                    </Link>
-                                                </li>
-                                            ))}
-                                        </ul>
-                                    </nav>
-                                </div>
+                            <div>
+                                <Offcanvas show={show} onHide={handleClose} responsive="lg">
+                                    <Offcanvas.Header  className=' px-4' closeButton>
+                                        <h1>LOGO</h1>
+                                    </Offcanvas.Header>
+                                    <Offcanvas.Body>
+                                        <div style={{ maxWidth: '200px' }}>
+                                            <p className="mb-0">
+                                                <div className=' px-2' >                                                   
+                                                    <div className="profile-img">
+                                                        <img src={require('../images/User-Profile-PNG-Image.png')} alt="" className="img-fluid rounded-circle" />
+                                                    </div>
+                                                    <h1 className="sitename text-capitalize">{(user !== null) ? (user.name) : ("admin")}</h1>
+                                                    <nav id="navmenu" className="navmenu">
+                                                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                                                            {props.navSections.map((section, i) => (
+                                                                <li key={i} className="nav-item">
+                                                                    <Link className={activeLink === section.secName ? "active" : ""}
+                                                                        onClick={() => setActiveLink(section.secName)}
+                                                                        to={section.linkTo} >
+                                                                        <i className={section.icon}></i>
+                                                                        {section.secName}
+                                                                    </Link>
+                                                                </li>
+                                                            ))}
+                                                        </ul>
+                                                    </nav>
+                                                </div>
+                                            </p>
+                                        </div>
+                                    </Offcanvas.Body>
+                                </Offcanvas>
                             </div>
                         )}
                     {/* offcanvas setion end */}
@@ -254,21 +177,10 @@ function Header(props) {
 
                     <div className='section-color h-100 flex-grow-1'>
                         <Outlet />
-                    </div>
-
+                    </div>                  
                     {/* pages section end  */}
 
                 </div>
-
-
-
-
-
-
-
-
-
-
             </div>
         </div >
     )
--- frontend/src/components/NewEmployee.js
+++ frontend/src/components/NewEmployee.js
@@ -1,10 +1,13 @@
 import { React, useState } from 'react';
-import Modal from 'react-modal';
+//import Modal from 'react-modal';
 import axios from 'axios'
+
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 
 function NewEmployee() {
-
+    const [show, setShow] = useState(false);
     const [modalIsOpen, setIsOpen] = useState(false);
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
@@ -12,7 +15,7 @@ function NewEmployee() {
     const [lastName, setLastName] = useState("");
     const [mobileNo, setMobileNo] = useState("");
     // const [LoginStatus, setLoginStatus] = useState("");
-    const [address, setAddress] = useState("");    
+    const [address, setAddress] = useState("");
 
     const customStyles = {
         content: {
@@ -47,7 +50,7 @@ function NewEmployee() {
     };
     const addEmployee = async () => {
         try {
-           
+
             const tokenStr = localStorage.getItem('token');
             const config = {
                 headers: {
@@ -67,61 +70,60 @@ function NewEmployee() {
     }
     return (
         <div>
-            <button className='btn m-5' onClick={openModal}>Add New Employee</button>
+            <Button variant="primary" onClick={() => setShow(true)}>
+               add new employee 
+            </Button>
+
             <Modal
-                isOpen={modalIsOpen}
-                onRequestClose={closeModal}
-                style={customStyles}
-                contentLabel="Example Modal" >
-                <div className="container mt-3 main-content ">
+                show={show}
+                onHide={() => setShow(false)}
+                dialogClassName="modal-90w"
+                aria-labelledby="example-custom-modal-styling-title"
+            >
+                <Modal.Header className='bg-color' closeButton>
+                    <Modal.Title id="example-custom-modal-styling-title">
+                    Welcome To Registration
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <div className="container mt-3 ">                       
 
-                    <div className="row p-5 bg-color rounded-border text-white">
-                    <div className="col-12 text-end">
-                            <button onClick={closeModal}>X</button>
-                        </div>
-                        <div className="col-12 ">
-                            <h2 className="text-center text-xl ">Welcome To Registration</h2>
-                        </div>
-                       
-                    </div >
-                    {/* <form > */}
-                    <div className="row bg-light p-5 p-sm-none ">
+                        {/* <div className="row bg-light p-5 p-sm-none "> */}
 
-                        <div className="col-sm-12 col-lg-4 form-group">
-                            <label>First Name</label>
-                            <input type="text" className="form-control" placeholder="First name." onChange={e => setFirstName(e.target.value)} required />
-                        </div>
-                        <div className="col-sm-12 col-lg-4 form-group">
-                            <label>Last Name</label>
-                            <input type="text" className="form-control" placeholder="Last name." onChange={e => setLastName(e.target.value)} required />
-                        </div>
-                        <div className="col-sm-12 col-lg-4 form-group">
-                            <label >Mobile No.</label>
-                            <input type="mobile" className="form-control" placeholder="Mobile No." onChange={e => setMobileNo(e.target.value)} required />
-                        </div>
-                        <div className="col-sm-12 form-group">
-                            <label >Address</label>
-                            <input type="address" className="form-control" placeholder="Address" onChange={e => setAddress(e.target.value)} required />
+                            <div className="col-sm-12 col-lg-4 form-group">
+                                <label>First Name</label>
+                                <input type="text" className="form-control" placeholder="First name." onChange={e => setFirstName(e.target.value)} required />
+                            </div>
+                            <div className="col-sm-12 col-lg-4 form-group">
+                                <label>Last Name</label>
+                                <input type="text" className="form-control" placeholder="Last name." onChange={e => setLastName(e.target.value)} required />
+                            </div>
+                            <div className="col-sm-12 col-lg-4 form-group">
+                                <label >Mobile No.</label>
+                                <input type="mobile" className="form-control" placeholder="Mobile No." onChange={e => setMobileNo(e.target.value)} required />
+                            </div>
+                            <div className="col-sm-12 form-group">
+                                <label >Address</label>
+                                <input type="address" className="form-control" placeholder="Address" onChange={e => setAddress(e.target.value)} required />
+                            </div>
+
+                            <div className="col-sm-12 form-group">
+                                <label >Email</label>
+                                <input type="email" className="form-control" placeholder="Enter your email." onChange={e => setEmail(e.target.value)} required />
+                            </div>
+
+                            <div className="col-sm-12 form-group">
+                                <label >Password</label>
+                                <input type="Password" name="password" className="form-control" placeholder="Enter your password." onChange={e => setPassword(e.target.value)} required />
+                            </div>
+                            <div className="col-sm-12 form-group mb-0">
+                                <button className="form-control btn bg-color btn-outline text-white" onClick={() => addEmployee()} >Submit</button>
+                            </div>
                         </div>
 
-                        <div className="col-sm-12 form-group">
-                            <label >Email</label>
-                            <input type="email" className="form-control" placeholder="Enter your email." onChange={e => setEmail(e.target.value)} required />
-                        </div>
-
-                        <div className="col-sm-12 form-group">
-                            <label >Password</label>
-                            <input type="Password" name="password" className="form-control" placeholder="Enter your password." onChange={e => setPassword(e.target.value)} required />
-                        </div>
-                        <div className="col-sm-12 form-group mb-0">
-                            <button className="form-control btn bg-color btn-outline text-white" onClick={() => addEmployee()} >Submit</button>
-                        </div>
-                    </div>
-                    {/* </form> */}
-                </div>
-
-
-            </Modal >
+                    {/* </div> */}
+                </Modal.Body>
+            </Modal>
         </div >
     )
 }
