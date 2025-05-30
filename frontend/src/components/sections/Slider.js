e3802f54389168ccb208cb7a30aed9a57e756e7a
Tejal Kadam <tejalmchandlekar@gmail.com>
2025-05-30 14:35:44 +0530

    added code for this marquee tag

--- frontend/src/components/sections/Slider.js
+++ frontend/src/components/sections/Slider.js
@@ -1,62 +1,43 @@
-import React from 'react'
-import image1 from '../../../src/images/asus.jpg'
-import mainImage from '../../../src/images/hero1.jpg'
-import mainImage1 from '../../../src/images/cta-background.jpg'
-import mainImage2 from '../../../src/images/image2.jpg'
-import ImageSlider from './ImageSlider'
+import { imageUrl1, imageUrl2, imageUrl3 } from './SliderImagesArray';
+
 import Marquee from "react-fast-marquee";
 function Slider() {
-    const images = [
-        {
-            id: "slider1",
-            images: [
-                mainImage,
-                mainImage1,
-                mainImage2
-            ],
-            direction: "left"
-        },
-        {
-            id: "slider2",
-            images: [
-                mainImage,
-                mainImage2,
-                mainImage1
-            ],
-            direction: "right"
-        },
-        {
-            id: "slider2",
-            images: [
-                mainImage1,
-                mainImage,
-                mainImage2
-            ],
-            direction: "left"
-        }
-
-        // Add more image URLs here
-    ];
     return (
         <section className='sliders-container'>
-            <Marquee>
-                <h1>fsdffd</h1>
-               <img src={image1} alt="no image" style={{width:"100px",height:"100px", backgroundColor:"red"}}/>
-               <img src={mainImage1} alt="no image" style={{width:"100px",height:"100px", backgroundColor:"red"}}/>
-               <img src={mainImage2} alt="no image" style={{width:"100px",height:"100px", backgroundColor:"red"}}/>
-               <img src={mainImage} alt="no image" style={{width:"100px",height:"100px", backgroundColor:"red"}}/>
+            <Marquee pauseOnHover="true" gradient="true">
+                {imageUrl1.map((section, i) => (
+                    <div class="image" style={{ "background-image": `url('${section.url}')` }}>
+                        <div class="overlay">
+                        </div>
+                    </div>
+
+                ))}
+
             </Marquee>
-              <Marquee direction='left'>
-                 {
-                    images.map((image) => (
-                        <image key={image.id} id={image.id} images={image.images} style={{height:"50px", width:"20%"}}/>
+            <Marquee direction='right' pauseOnHover="true" gradient="true">
+                {
+                    imageUrl2.map((section, i) => (
+                        <div class="image" style={{ "background-image": `url('${section.url}')` }}>
+                            <div class="overlay">
+                            </div>
+                        </div>
+
                     ))
+
                 }
             </Marquee>
-            {/* <div>
-                <ImageSlider images={images} />
-            </div> */}
+            <Marquee pauseOnHover="true" gradient="true">
+                {
+                    imageUrl3.map((section, i) => (
+                        <div class="image" style={{ "background-image": `url('${section.url}')` }}>
+                            <div class="overlay">
+                            </div>
+                        </div>
 
+                    ))
+
+                }
+            </Marquee>
             {/* <div class="row d-flex align-items-center justify-content-between">
                     <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 d-none d-md-block">
                         <div class="hero__img" data-animation="bounceIn" data-delay=".4s" >
---
+++ frontend/src/components/sections/SliderImagesArray.js
@@ -0,0 +1,80 @@
+export const imageUrl1 = [
+    {
+        url: 'http://res.cloudinary.com/dknm0xjjo/image/upload/v1748333877/productImg/1748333873378-302609175.jpg'
+    },
+    {
+        url:'https://res.cloudinary.com/dknm0xjjo/image/upload/v1748506593/productImg/1748506590480-80160850.jpg'
+    },
+    {
+        url:'https://res.cloudinary.com/dknm0xjjo/image/upload/v1748506664/productImg/1748506659560-11045645.jpg'
+    },
+    {
+        url:'https://res.cloudinary.com/dknm0xjjo/image/upload/v1748505000/productImg/1748504998724-835180783.jpg'
+    },
+    {
+        url:'https://res.cloudinary.com/dknm0xjjo/image/upload/v1748503009/productImg/1748503004050-836963701.jpg'
+    },
+    {
+        url:'https://res.cloudinary.com/dknm0xjjo/image/upload/v1748502976/productImg/1748502975267-906177545.jpg'
+    },
+     {
+        url:'https://res.cloudinary.com/dknm0xjjo/image/upload/v1748421187/productImg/1748421185591-169607479.jpg'
+    },
+    {
+        url:'https://res.cloudinary.com/dknm0xjjo/image/upload/v1748502743/productImg/1748502741700-503536501.jpg'
+    },  
+]
+
+export const imageUrl2 = [
+    {
+        url: 'http://res.cloudinary.com/dknm0xjjo/image/upload/v1748333877/productImg/1748333873378-302609175.jpg'
+    },
+    {
+        url:'https://res.cloudinary.com/dknm0xjjo/image/upload/v1748506593/productImg/1748506590480-80160850.jpg'
+    },
+    {
+        url:'https://res.cloudinary.com/dknm0xjjo/image/upload/v1748506664/productImg/1748506659560-11045645.jpg'
+    },
+    {
+        url:'https://res.cloudinary.com/dknm0xjjo/image/upload/v1748505000/productImg/1748504998724-835180783.jpg'
+    },
+    {
+        url:'https://res.cloudinary.com/dknm0xjjo/image/upload/v1748503009/productImg/1748503004050-836963701.jpg'
+    },
+    {
+        url:'https://res.cloudinary.com/dknm0xjjo/image/upload/v1748502976/productImg/1748502975267-906177545.jpg'
+    },
+     {
+        url:'https://res.cloudinary.com/dknm0xjjo/image/upload/v1748421187/productImg/1748421185591-169607479.jpg'
+    },
+    {
+        url:'https://res.cloudinary.com/dknm0xjjo/image/upload/v1748502743/productImg/1748502741700-503536501.jpg'
+    },  
+]
+export const imageUrl3 = [
+    {
+        url: 'http://res.cloudinary.com/dknm0xjjo/image/upload/v1748333877/productImg/1748333873378-302609175.jpg'
+    },
+    {
+        url:'https://res.cloudinary.com/dknm0xjjo/image/upload/v1748506593/productImg/1748506590480-80160850.jpg'
+    },
+    {
+        url:'https://res.cloudinary.com/dknm0xjjo/image/upload/v1748506664/productImg/1748506659560-11045645.jpg'
+    },
+    {
+        url:'https://res.cloudinary.com/dknm0xjjo/image/upload/v1748505000/productImg/1748504998724-835180783.jpg'
+    },
+    {
+        url:'https://res.cloudinary.com/dknm0xjjo/image/upload/v1748503009/productImg/1748503004050-836963701.jpg'
+    },
+    {
+        url:'https://res.cloudinary.com/dknm0xjjo/image/upload/v1748502976/productImg/1748502975267-906177545.jpg'
+    },
+     {
+        url:'https://res.cloudinary.com/dknm0xjjo/image/upload/v1748421187/productImg/1748421185591-169607479.jpg'
+    },
+    {
+        url:'https://res.cloudinary.com/dknm0xjjo/image/upload/v1748502743/productImg/1748502741700-503536501.jpg'
+    },  
+]
+
