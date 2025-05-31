bb2c94cfd4e51522331c2ff20e6ad01740c062aa
Tejal Kadam <tejalmchandlekar@gmail.com>
2025-05-31 12:57:53 +0530

    setting of images for the marquee tag in the website

--- frontend/src/components/sections/SliderImagesArray.js
+++ frontend/src/components/sections/SliderImagesArray.js
@@ -6,72 +6,73 @@ export const imageUrl1 = [
         url:'https://res.cloudinary.com/dknm0xjjo/image/upload/v1748506593/productImg/1748506590480-80160850.jpg'
     },
     {
-        url:'https://res.cloudinary.com/dknm0xjjo/image/upload/v1748506664/productImg/1748506659560-11045645.jpg'
+        url:'https://res.cloudinary.com/dknm0xjjo/image/upload/v1748422850/productImg/1748422842074-262519534.jpg'
     },
     {
-        url:'https://res.cloudinary.com/dknm0xjjo/image/upload/v1748505000/productImg/1748504998724-835180783.jpg'
+        url:'https://res.cloudinary.com/dknm0xjjo/image/upload/v1748422801/productImg/1748422797749-33439461.jpg'
     },
     {
         url:'https://res.cloudinary.com/dknm0xjjo/image/upload/v1748503009/productImg/1748503004050-836963701.jpg'
     },
     {
-        url:'https://res.cloudinary.com/dknm0xjjo/image/upload/v1748502976/productImg/1748502975267-906177545.jpg'
+        url:'https://res.cloudinary.com/dknm0xjjo/image/upload/v1748506498/productImg/1748506494948-113885606.jpg'
     },
      {
-        url:'https://res.cloudinary.com/dknm0xjjo/image/upload/v1748421187/productImg/1748421185591-169607479.jpg'
+        url:'https://res.cloudinary.com/dknm0xjjo/image/upload/v1748502678/productImg/1748502677619-455347614.jpg'
     },
     {
-        url:'https://res.cloudinary.com/dknm0xjjo/image/upload/v1748502743/productImg/1748502741700-503536501.jpg'
+        url:'https://res.cloudinary.com/dknm0xjjo/image/upload/v1748505653/productImg/1748505651994-810744515.jpg'
     },  
 ]
 
 export const imageUrl2 = [
     {
-        url: 'http://res.cloudinary.com/dknm0xjjo/image/upload/v1748333877/productImg/1748333873378-302609175.jpg'
+        url:'https://res.cloudinary.com/dknm0xjjo/image/upload/v1748334118/productImg/1748334116348-399075308.jpg'
     },
     {
-        url:'https://res.cloudinary.com/dknm0xjjo/image/upload/v1748506593/productImg/1748506590480-80160850.jpg'
+        url: 'https://res.cloudinary.com/dknm0xjjo/image/upload/v1748419828/productImg/1748419826130-817852196.jpg'
     },
     {
-        url:'https://res.cloudinary.com/dknm0xjjo/image/upload/v1748506664/productImg/1748506659560-11045645.jpg'
+        url:'https://res.cloudinary.com/dknm0xjjo/image/upload/v1748506330/productImg/1748506326386-592554479.jpg'
     },
     {
-        url:'https://res.cloudinary.com/dknm0xjjo/image/upload/v1748505000/productImg/1748504998724-835180783.jpg'
+        url:'https://res.cloudinary.com/dknm0xjjo/image/upload/v1748417411/productImg/1748417409587-368926403.jpg'
     },
     {
-        url:'https://res.cloudinary.com/dknm0xjjo/image/upload/v1748503009/productImg/1748503004050-836963701.jpg'
+        url:'https://res.cloudinary.com/dknm0xjjo/image/upload/v1748335943/productImg/1748335938867-91872010.jpg'
     },
     {
-        url:'https://res.cloudinary.com/dknm0xjjo/image/upload/v1748502976/productImg/1748502975267-906177545.jpg'
+        url:'https://res.cloudinary.com/dknm0xjjo/image/upload/v1748336000/productImg/1748335997023-790688176.jpg'
+    },
+    {
+        url:'https://res.cloudinary.com/dknm0xjjo/image/upload/v1748334585/productImg/1748334582069-356952934.jpg'
     },
      {
-        url:'https://res.cloudinary.com/dknm0xjjo/image/upload/v1748421187/productImg/1748421185591-169607479.jpg'
+        url:'https://res.cloudinary.com/dknm0xjjo/image/upload/v1748506786/productImg/1748506764470-349088141.jpg'
     },
-    {
-        url:'https://res.cloudinary.com/dknm0xjjo/image/upload/v1748502743/productImg/1748502741700-503536501.jpg'
-    },  
+      
 ]
 export const imageUrl3 = [
     {
-        url: 'http://res.cloudinary.com/dknm0xjjo/image/upload/v1748333877/productImg/1748333873378-302609175.jpg'
+        url: 'https://res.cloudinary.com/dknm0xjjo/image/upload/v1748506452/productImg/1748506451161-522361663.jpg'
     },
     {
-        url:'https://res.cloudinary.com/dknm0xjjo/image/upload/v1748506593/productImg/1748506590480-80160850.jpg'
+        url:'https://res.cloudinary.com/dknm0xjjo/image/upload/v1748505597/productImg/1748505593223-657356417.jpg'
     },
     {
         url:'https://res.cloudinary.com/dknm0xjjo/image/upload/v1748506664/productImg/1748506659560-11045645.jpg'
     },
     {
-        url:'https://res.cloudinary.com/dknm0xjjo/image/upload/v1748505000/productImg/1748504998724-835180783.jpg'
+        url:'https://res.cloudinary.com/dknm0xjjo/image/upload/v1748506297/productImg/1748506293530-33915705.jpg'
     },
     {
         url:'https://res.cloudinary.com/dknm0xjjo/image/upload/v1748503009/productImg/1748503004050-836963701.jpg'
     },
     {
-        url:'https://res.cloudinary.com/dknm0xjjo/image/upload/v1748502976/productImg/1748502975267-906177545.jpg'
+        url:'https://res.cloudinary.com/dknm0xjjo/image/upload/v1748506730/productImg/1748506728744-108503435.jpg'
     },
      {
-        url:'https://res.cloudinary.com/dknm0xjjo/image/upload/v1748421187/productImg/1748421185591-169607479.jpg'
+        url:'https://res.cloudinary.com/dknm0xjjo/image/upload/v1748506412/productImg/1748506409992-168905223.jpg'
     },
     {
         url:'https://res.cloudinary.com/dknm0xjjo/image/upload/v1748502743/productImg/1748502741700-503536501.jpg'
